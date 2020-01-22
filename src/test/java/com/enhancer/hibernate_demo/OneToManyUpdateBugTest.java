ca368d638c44e0cd72ec26101c51445e0fc7a9c6
Enhancer <xjhxyshz@126.com>
2020-01-22 21:59:03 +0800

    add solution

--- src/test/java/com/enhancer/hibernate_demo/OneToManyUpdateBugTest.java
+++ src/test/java/com/enhancer/hibernate_demo/OneToManyUpdateBugTest.java
@@ -8,8 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.Set;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @Slf4j
 @SpringBootTest
@@ -53,6 +58,44 @@ public class OneToManyUpdateBugTest {
             one.getManySet().add(newMany);
 
             entityManager.flush(); // Throws ConstraintViolationException: NULL not allowed for column "ONE_ID"
+        });
+    }
+
+    @Test
+    public void solutionTest() {
+        doInJPA(() -> entityManagerFactory, entityManager -> {
+            One one = new One(1L);
+            Many many = new Many();
+            many.setOneId(one.getId());
+            many.setValue(1);
+            one.getManySet().add(many);
+            entityManager.persist(one);
+            log.info("First unit of work done");
+        });
+
+        doInJPA(() -> entityManagerFactory, entityManager -> {
+            One one = entityManager.find(One.class, 1L);
+            Many existingMany = one.getManySet().iterator().next();
+            existingMany.setValue(2);
+            log.info("Third unit of work, updating done");
+
+            Many newMany = new Many();
+            newMany.setOneId(one.getId());
+            entityManager.persist(newMany);
+        });
+
+        doInJPA(() -> entityManagerFactory, entityManager -> {
+            One one = entityManager.find(One.class, 1L);
+            Set<Many> manySet = one.getManySet();
+            assertThat(manySet, hasSize(2));
+            for (Many many : manySet) {
+                if (many.getId().equals(1L)) {
+                    assertThat(many.getValue(), is(2));
+                } else if (many.getId().equals(2L)) {
+                } else {
+                    fail();
+                }
+            }
         });
     }
 }
